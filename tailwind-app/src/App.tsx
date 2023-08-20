66346bbdec1e0bcd5f2767585beaf5bd6531dd32
daniel <danielemmanuel257@gmail.com>
2023-08-20 19:19:47 +0100

    added react to the project

---
+++ tailwind-app/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  // Sm, md, lg, xl 2xl
+
+  // dark
+
+  return (
+  <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-64 h-64 rounded-lg shadow-lg bg-[black] dark:bg-white"></div>
+  </div>
+  )
+}
+
+export default App
